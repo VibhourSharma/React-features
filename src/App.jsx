5d43a430d905c87c796b4d688a9381813c88d926
Vibhour Sharma <vibhour121@gmail.com>
2024-01-05 23:54:58 +0530

    🚀Day3: Slider ✅

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Accordion from "./components/Accordion/Accordion";
 import Modal from "./components/Modal/Modal";
+import Slider from "./components/Slider/Slider";
 
 function App() {
   return (
     <>
       <Accordion />
       <Modal />
+      <Slider />
     </>
   );
 }
